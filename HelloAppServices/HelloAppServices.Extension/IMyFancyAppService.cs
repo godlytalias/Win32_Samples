fce97f1018b311a07f5240941fc738298d94941e
Godly Alias <godlyalias@microsoft.com>
2024-08-10 04:53:30 +0530

    Added app service sample
    Signed-off-by: Godly Alias <godlyalias@microsoft.com>

---
+++ HelloAppServices/HelloAppServices.Extension/IMyFancyAppService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.AppServices;
+
+namespace HelloAppServices.Extension
+{
+    [AppService("MyFancyAppService")]
+    public interface IMyFancyAppService
+    {
+        Task<int> SumAsync(int x, int y);
+    }
+}
---
+++ HelloAppServices/HelloAppServices.Extension/MyFancyAppService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.AppServices;
+using HelloAppServices.Extension;
+
+namespace HelloAppServices.Extension
+{
+    public sealed partial class MyFancyAppService : IMyFancyAppService
+    {
+        public async Task<int> SumAsync(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
